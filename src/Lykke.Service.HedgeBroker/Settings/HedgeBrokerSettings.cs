f497d878cb6e850f1bbdc54411abc3fab9e808d9
Petr Giloian <giloyanp@gmail.com>
2018-09-13 19:46:45 +0700

    Initial commit.

---
+++ src/Lykke.Service.HedgeBroker/Settings/HedgeBrokerSettings.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Annotations;
+using Lykke.SettingsReader.Attributes;
+
+namespace Lykke.Service.HedgeBroker.Settings
+{
+    [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+    public class HedgeBrokerSettings
+    {
+        public DbSettings Db { get; set; }
+    }
+}
