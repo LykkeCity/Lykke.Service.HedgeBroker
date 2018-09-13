b1f68981df10e36dd5cdc6f9bba25875d6a9079b
Petr Giloian <giloyanp@gmail.com>
2018-09-13 20:46:49 +0700

    Initial commit.

---
+++ client/Lykke.Service.HedgeBroker.Client/HedgeBrokerClient.cs
@@ -0,0 +1,21 @@
+﻿using Lykke.HttpClientGenerator;
+
+namespace Lykke.Service.HedgeBroker.Client
+{
+    /// <summary>
+    /// HedgeBroker API aggregating interface.
+    /// </summary>
+    public class HedgeBrokerClient : IHedgeBrokerClient
+    {
+        // Note: Add similar Api properties for each new service controller
+
+        /// <summary>Inerface to HedgeBroker Api.</summary>
+        public IHedgeBrokerApi Api { get; private set; }
+
+        /// <summary>C-tor</summary>
+        public HedgeBrokerClient(IHttpClientGenerator httpClientGenerator)
+        {
+            Api = httpClientGenerator.Generate<IHedgeBrokerApi>();
+        }
+    }
+}
---
+++ client/Lykke.Service.HedgeBroker.Client/HedgeBrokerServiceClientSettings.cs
@@ -0,0 +1,14 @@
+﻿using Lykke.SettingsReader.Attributes;
+
+namespace Lykke.Service.HedgeBroker.Client 
+{
+    /// <summary>
+    /// HedgeBroker client settings.
+    /// </summary>
+    public class HedgeBrokerServiceClientSettings 
+    {
+        /// <summary>Service url.</summary>
+        [HttpCheck("api/isalive")]
+        public string ServiceUrl {get; set;}
+    }
+}
